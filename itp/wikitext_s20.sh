01dcf1a3f1974c9de7c82e5241ed903870f8ed79
jiahangxu <jiahangxu@microsoft.com>
2023-08-21 01:07:54 +0800

    fix lr bug and add submit scripts

--- itp/wikitext_s20.sh
+++
@@ -1,5 +0,0 @@
-python run_sing.py submit --sparsity 0.2  --target sing_octo --task_name wikitext2 --model_name llama7b_wolayer-s20.0-lr5e-05-reglr0.1-warmup20 --file eval_wikitext2_llama7b_ft
-python run_sing.py submit --sparsity 0.2  --target sing_octo --task_name piqa --model_name llama7b_wolayer-s20.0-lr5e-05-reglr0.1-warmup20 --file eval_piqa_llama7b_wolayer-s20.0-lr5e-05-reglr0.1-warmup20
-python run_sing.py submit --sparsity 0.2  --target sing_octo --task_name arc-e --model_name llama7b_wolayer-s20.0-lr5e-05-reglr0.1-warmup20 --file eval_arce_llama7b_wolayer-s20.0-lr5e-05-reglr0.1-warmup20
-python run_sing.py submit --sparsity 0.2  --target sing_octo --task_name arc-c --model_name llama7b_wolayer-s20.0-lr5e-05-reglr0.1-warmup20 --file eval_arcc_llama7b_wolayer-s20.0-lr5e-05-reglr0.1-warmup20
-python run_sing.py submit --sparsity 0.2  --target sing_octo --task_name storycloze --model_name llama7b_wolayer-s20.0-lr5e-05-reglr0.1-warmup20 --file eval_storycloze_llama7b_wolayer-s20.0-lr5e-05-reglr0.1-warmup20
