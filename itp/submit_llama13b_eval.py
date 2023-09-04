2a7e54ad3a28e3288a09a870f9bbc5de2f24b7f8
jiahangxu <jiahangxu@microsoft.com>
2023-09-04 20:17:37 +0800

    update c4 eval

--- itp/submit_llama13b_eval.py
+++ itp/submit_llama13b_eval.py
@@ -10,19 +10,21 @@ def single_job(arg):
             mark = mrk + f"_prompt_type{prompt_type}"
         command_head = "python run_sing.py submit --target sing_research --model_name eval_llama13b "
         command_list_no_prompt = [
-            command_head + f"--file evaluation_llama13b/zeroshot/eval_llama7b_boolqa.sh --task_name boolqa --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
-            command_head + f"--file evaluation_llama13b/zeroshot/eval_llama7b_piqa.sh --task_name piqa --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
+            # command_head + f"--file evaluation_llama13b/zeroshot/eval_llama7b_boolqa.sh --task_name boolqa --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
+            # command_head + f"--file evaluation_llama13b/zeroshot/eval_llama7b_piqa.sh --task_name piqa --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
             # command_head + f"--file evaluation_llama13b/zeroshot/eval_llama7b_hellaswag.sh --task_name hellaswag --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 8",
         ]
         command_list = [
+            command_head + f"--file evaluation_llama13b/eval_harness.sh --task_name harness --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
             command_head + f"--file evaluation_llama13b/eval_wikitext.sh --task_name wikitext2_eval --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
-            command_head + f"--file evaluation_llama13b/zeroshot/eval_llama7b_piqa.sh --task_name piqa --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
-            command_head + f"--file evaluation_llama13b/zeroshot/eval_llama7b_storycloze.sh --task_name storycloze --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
-            command_head + f"--file evaluation_llama13b/zeroshot/eval_llama7b_arcc.sh --task_name arc-c --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
-            command_head + f"--file evaluation_llama13b/zeroshot/eval_llama7b_arce.sh --task_name arc-e --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
-            command_head + f"--file evaluation_llama13b/zeroshot/eval_llama7b_winogrande.sh --task_name winogrande --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
+            command_head + f"--file evaluation_llama13b/eval_c4.sh --task_name c4 --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
+            # command_head + f"--file evaluation_llama13b/zeroshot/eval_llama7b_piqa.sh --task_name piqa --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
+            # command_head + f"--file evaluation_llama13b/zeroshot/eval_llama7b_storycloze.sh --task_name storycloze --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
+            # command_head + f"--file evaluation_llama13b/zeroshot/eval_llama7b_arcc.sh --task_name arc-c --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
+            # command_head + f"--file evaluation_llama13b/zeroshot/eval_llama7b_arce.sh --task_name arc-e --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
+            # command_head + f"--file evaluation_llama13b/zeroshot/eval_llama7b_winogrande.sh --task_name winogrande --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
             # command_head + f"--file evaluation_llama13b/zeroshot/eval_llama7b_boolqa.sh --task_name boolqa --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
-            command_head + f"--file evaluation_llama13b/zeroshot/eval_llama7b_obqa.sh --task_name obqa --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
+            # command_head + f"--file evaluation_llama13b/zeroshot/eval_llama7b_obqa.sh --task_name obqa --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4",
             # command_head + f"--file evaluation_llama13b/zeroshot/eval_llama7b_hellaswag.sh --task_name hellaswag --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 8",
             # command_head + f"--file evaluation_llama13b/eval_gsm8k_train.sh --task_name gsm8k --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 8",
             # command_head + f"--file evaluation_llama13b/eval_multiarith.sh --task_name multiarith --ckpt_dir /mnt/data/{ckpt_path} --prompt_type {prompt_type} --mark {mark} --node_num 4", 
@@ -41,7 +43,7 @@ waiting_jobs = [
     
     ## ================= fix LR scheduler bug ================= ##
     # # mark 36
-    # ("LoRaPruner/gpt4alpaca_llama13b_prompt_nogate-s20.0-lr5e-05-reglr0.05-warmup4/2023-8-23-5-24", "mark36_epoch5", 1),
+    ("LoRaPruner/gpt4alpaca_llama13b_prompt_nogate-s20.0-lr5e-05-reglr0.05-warmup4/2023-8-23-5-24/epoch6", "mark36_epoch6", 1),
 
     # # mark 37
     # ("LoRaPruner/gpt4alpaca_llama13b_closeinit_gate_0.5lagST-s20.0-lr5e-05-reglr0.05-warmup4/2023-8-21-9-58", "mark37_epoch5", 1),
